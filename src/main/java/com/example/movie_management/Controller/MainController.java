163b6e05efbf4a4a0a46b47d77a32cc6f8d5265b
Berkay Cekmez <xiorusss@gmail.com>
2024-03-04 14:32:30 +0300

    update2

---
+++ src/main/java/com/example/movie_management/Controller/MainController.java
@@ -0,0 +1,10 @@
+package com.example.movie_management.Controller;
+import javafx.fxml.FXML;
+import javafx.scene.control.TabPane;
+
+public class MainController {
+
+    @FXML
+    private TabPane tabPane;
+
+}
